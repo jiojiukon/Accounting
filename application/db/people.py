5e09de45dc83a3b17657221d8b32ccb37d506032
Evgeniy Osipov <z624153@mail.ru>
2022-11-22 19:34:04 +0400

    start

---
+++ application/db/people.py
@@ -0,0 +1,10 @@
+employees_list = ['Aleksey', 'Christina', 'Elena']
+
+
+def get_employees(my_list):
+    for employee in my_list:
+        print(employee)
+
+
+if __name__ == '__main__':
+    get_employees(employees_list)
